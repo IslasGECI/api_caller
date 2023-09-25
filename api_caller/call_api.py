3b2afb592b847aa54cdf098cb3f7684ba0a1c224
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-25 20:43:32 +0000

    ✅ Pass tests

--- api_caller/call_api.py
+++ api_caller/call_api.py
@@ -1,2 +1,2 @@
-def construct_entrypoint_url(service_name, port=10000):
-    return f"http://{service_name}:{port}"
+def construct_entrypoint_url(service_name, port=10000, entrypoint_name=""):
+    return f"http://{service_name}:{port}{entrypoint_name}"

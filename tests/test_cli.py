90733572a14139fd60c17b5aee9726cfa684499f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-25 21:28:22 +0000

    🛑🧪 Fail tests

---
+++ tests/test_cli.py
@@ -0,0 +1,11 @@
+from api_caller import cli
+
+from typer.testing import CliRunner
+
+runner = CliRunner()
+
+
+def test_call_entrypoint():
+    result = runner.invoke(cli, "--help")
+
+    assert result.exit_code == 0

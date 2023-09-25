4636daf8822f9764c9e82259f5f913f2a3c6023c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-25 20:41:17 +0000

    ♻️  Refactor

--- tests/test_call_api.py
+++ tests/test_call_api.py
@@ -4,11 +4,11 @@ from api_caller import construct_entrypoint_url
 def test_construct_entrypoint_url():
     service_name = "eradication_progress"
     obtained_url = construct_entrypoint_url(service_name)
-    expect_url = "https://eradication_progress:10000"
+    expect_url = "http://eradication_progress:10000"
     assert obtained_url == expect_url
 
     port = 10001
     service_name = "edr_api"
     obtained_url = construct_entrypoint_url(service_name, port)
-    expect_url = "https://edr_api:10001"
+    expect_url = "http://edr_api:10001"
     assert obtained_url == expect_url

62160776fb5358fd3d002a51852cf5f973e25749
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-25 21:52:16 +0000

    ✅ Pass tests

--- api_caller/cli.py
+++ api_caller/cli.py
@@ -4,5 +4,9 @@ cli = typer.Typer()
 
 
 @cli.command()
-def call(service_name: str = typer.Option(help=""), port: int = typer.Option(help="")):
+def call(
+    service_name: str = typer.Option(help=""),
+    port: int = typer.Option(help=""),
+    entrypoint_name: str = typer.Option(help=""),
+):
     pass

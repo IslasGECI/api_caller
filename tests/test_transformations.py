549d8d2a0c940e8ff93622d5ed803fe3c3bd3f79
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-09-25 13:23:27 -0700

    🔥⚰️ Remove example tests

--- tests/test_transformations.py
+++
@@ -1,9 +0,0 @@
-import api_caller as dt
-
-
-def test_add_offset():
-    augend = 1
-    addend = 2
-    expected = augend + addend
-    obtained = dt.add_offset(augend, addend)
-    assert expected == obtained

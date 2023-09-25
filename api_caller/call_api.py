2f303deb2e8eee59ca12912153edfa0974421aa1
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-25 21:12:49 +0000

    ♻️  Refactor

--- api_caller/call_api.py
+++ api_caller/call_api.py
@@ -1,8 +1,5 @@
 def construct_entrypoint_url(service_name, port=10000, entrypoint_name="", **kwargs):
     base_url = f"http://{service_name}:{port}{entrypoint_name}"
-    print(kwargs)
-    if len(kwargs.items()) == 0:
-        return base_url
     options = "?"
     for key, value in kwargs.items():
         options += str(key) + "=" + str(value) + "&"

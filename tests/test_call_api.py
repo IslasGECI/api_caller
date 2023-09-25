c7336304eb546eace2e726f5c8d389a4a7af8810
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-25 20:35:15 +0000

    🛑🧪 Fail tests

--- tests/test_call_api.py
+++ tests/test_call_api.py
@@ -4,5 +4,10 @@ from api_caller import construct_entrypoint_url
 def test_construct_entrypoint_url():
     service_name = "eradication_progress"
     obtained_url = construct_entrypoint_url(service_name)
-    expect_url = "https://eradication_process"
+    expect_url = "https://eradication_progress"
+    assert obtained_url == expect_url
+
+    service_name = "edr_api"
+    obtained_url = construct_entrypoint_url(service_name)
+    expect_url = "https://edr_api"
     assert obtained_url == expect_url

e0a6ec96a17c6dd251f1d29c0d9cfdf2ad4899b8
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-25 21:16:28 +0000

    ♻️  Refactor

--- tests/test_call_api.py
+++ tests/test_call_api.py
@@ -17,3 +17,9 @@ def test_construct_entrypoint_url():
     obtained_url = construct_entrypoint_url(service_name, port, entrypoint_name, option_one="first")
     expect_url = "http://edr_api:10001/write_csv?option_one=first"
     assert obtained_url == expect_url
+
+    entrypoint_name = "/write_csv"
+    options = {"uno": 1, "dos": "dos"}
+    obtained_url = construct_entrypoint_url(service_name, port, entrypoint_name, **options)
+    expect_url = "http://edr_api:10001/write_csv?uno=1&dos=dos"
+    assert obtained_url == expect_url

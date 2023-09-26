7a81d2d6d13cad6957588ba6ee358d4ed3dd4f77
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-26 19:51:34 +0000

    ✅ Pass tests

--- api_caller/cli.py
+++ api_caller/cli.py
@@ -7,7 +7,7 @@ cli = typer.Typer()
 
 @cli.command()
 def write_csv_probability(
-    input_path: str = typer.Option(help=""),
+    input_path: str = typer.Option(help="Path of input data"),
     bootstrapping_number: int = typer.Option(help=""),
     output_path: str = typer.Option(help=""),
     window_length: int = typer.Option(help=""),

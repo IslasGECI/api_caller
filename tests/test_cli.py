e4b3c0992af0159336da6f1db549a7917e4832f2
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-26 17:44:11 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,8 +9,12 @@ def test_call_entrypoint():
     result = runner.invoke(cli, "--help")
     assert result.exit_code == 0
 
-    result = runner.invoke(cli, ["call", "--help"])
+    result = runner.invoke(cli, ["write-csv-probability", "--help"])
     assert result.exit_code == 0
     assert "--service-name " in result.stdout
     assert "--port " in result.stdout
     assert "--entrypoint-name " in result.stdout
+    assert "--input-path " in result.stdout
+    assert "--bootstrapping-number " in result.stdout
+    assert "--output-path " in result.stdout
+    assert "--window-length " in result.stdout

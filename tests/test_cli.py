209c666333d152f1be9e116e2d992ce5bd57c7d4
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-27 16:45:08 +0000

    🎨 Format with black

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -32,7 +32,10 @@ def test_call_entrypoint():
 
 def tests_write_csv_probability_entrypoint():
     with requests_mock.Mocker() as m:
-        m.get("http://eradication_progress:10000/write_effort_and_captures_with_probability", text="Response 200")
+        m.get(
+            "http://eradication_progress:10000/write_effort_and_captures_with_probability",
+            text="Response 200",
+        )
 
         runner.invoke(
             cli,
@@ -52,6 +55,7 @@ def tests_write_csv_probability_entrypoint():
         expected_url = "http://eradication_progress:10000/write_effort_and_captures_with_probability?input_path=effort_captures.csv&bootstrapping_number=2&output_path=probabilities.csv&window_length=6"
         print(m.request_history[0].url)
         assert m.request_history[0].url == expected_url
+
 
 def tests_write_probability_figure_entrypoint():
     with requests_mock.Mocker() as m:

423f802b585a5e7d253bac5d562487205ce2bb1e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-26 20:18:26 +0000

    ♻️  Refactor

--- geci_caller/cli.py
+++ geci_caller/cli.py
@@ -65,14 +65,15 @@ def plot_custom_cpue_vs_cum_captures(
     config_path: str = typer.Option(help="Path of config file"),
     output_path: str = typer.Option(help="Path of figure to write"),
 ):
-    construct_and_get(input_path, config_path, output_path)
+    entrypoint_name = "/plot_custom_cpue_vs_cum_captures"
+    construct_and_get(entrypoint_name, input_path, config_path, output_path)
 
 
-def construct_and_get(input_path, config_path, output_path):
+def construct_and_get(entrypoint_name, input_path, config_path, output_path):
     url = construct_entrypoint_url(
         "eradication_progress",
         10000,
-        "/plot_custom_cpue_vs_cum_captures",
+        entrypoint_name,
         input_path=input_path,
         config_path=config_path,
         output_path=output_path,

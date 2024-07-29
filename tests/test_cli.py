82cd7c4efca3d33c5324b335d78c1f4f3037a49a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-29 20:00:39 +0000

    ♻️  Refactor

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -78,7 +78,9 @@ def tests_write_csv_probability_entrypoint():
 
 def tests_write_probability_figure_entrypoint():
     with requests_mock.Mocker() as m:
-        runner.invoke(
+        entrypoint = "http://eradication_progress:10000/write_probability_figure"
+        m.get(entrypoint)
+        result = runner.invoke(
             cli,
             [
                 "write-probability-progress-figure",
@@ -90,6 +92,7 @@ def tests_write_probability_figure_entrypoint():
         )
         assert m.call_count == 1
         expected_url = "http://eradication_progress:10000/write_probability_figure?input_path=probabilities.csv&output_path=figure.png"
+        assert "200" in result.stdout
 
 
 def tests_plot_cumulative_series_cpue_by_flight_entrypoint():
@@ -97,7 +100,9 @@ def tests_plot_cumulative_series_cpue_by_flight_entrypoint():
     assert_command_with_input_and_output_paths(result)
 
     with requests_mock.Mocker() as m:
-        runner.invoke(
+        entrypoint = "http://eradication_progress:10000/plot_cumulative_series_cpue_by_flight"
+        m.get(entrypoint)
+        result = runner.invoke(
             cli,
             [
                 "plot-cumulative-series-cpue-by-flight",
@@ -108,7 +113,7 @@ def tests_plot_cumulative_series_cpue_by_flight_entrypoint():
             ],
         )
         assert m.call_count == 1
-        expected_url = "http://eradication_progress:10000/plot_cumulative_series_cpue_by_flight?input_path=probabilities.csv&output_path=figure.png"
+        assert "200" in result.stdout
 
 
 def tests_plot_cpue_vs_cum_captures_entrypoint():

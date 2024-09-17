b5afd4d2f0095783196f85ba22917d9de77d08d2
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-17 21:56:40 +0000

    ✅ Pass tests

--- geci_caller/cli.py
+++ geci_caller/cli.py
@@ -6,6 +6,21 @@ cli = typer.Typer()
 
 
 @cli.command()
+def filter_by_method(
+    input_path: str = typer.Option(help="Path of input data"),
+    method: str = typer.Option(help="Extraction method"),
+    output_path: str = typer.Option(help="Path of figure to write"),
+):
+    entrypoint_name = "/filter_by_method"
+    get_eradication_progress(
+        entrypoint_name,
+        input_path=input_path,
+        method=method,
+        output_path=output_path,
+    )
+
+
+@cli.command()
 def write_population_status(
     input_path: str = typer.Option(help="Path of input data"),
     bootstrapping_number: int = typer.Option(help="Number of bootstraps"),

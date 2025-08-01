02209314d656d8cd0e4971112923c97f3aaa6e4e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-01 19:28:05 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -337,6 +337,7 @@ def tests_plot_custom_cpue_vs_cum_captures_entrypoint():
 def tests_version():
     obtained_result = get_command_help("version")
     assert_successful_command(obtained_result)
+    obtained_result = runner.invoke(cli, ["version"])
     assert "0.16.0" in obtained_result.stdout
 
 

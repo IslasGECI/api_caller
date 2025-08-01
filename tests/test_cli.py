89a72afb30920a20c6baf72851da594e24166632
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-01 19:06:19 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -290,6 +290,7 @@ def tests_plot_cpue_vs_cum_captures_entrypoint():
         ],
     )
     assert result.exit_code == 0
+    assert "200" in result.stdout
 
     response = plot_cpue_vs_cum_captures(input_path, output_path)
     assert "http://islasgeci.org:100/plot_cpue_vs_cum_captures" in response.url

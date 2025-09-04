976754f8fd03e592445f1eaf09eb5dab65b9e678
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-09-04 16:13:46 +0000

    ✅ Pass tests

--- geci_caller/cli.py
+++ geci_caller/cli.py
@@ -14,7 +14,16 @@ def plot_cumulative_cpue_series_by_season(
     input_path: str = typer.Option(help="Path of input data"),
     output_path: str = typer.Option(help="Path of figure to write"),
 ):
-    pass
+    url = "http://islasgeci.org:100/plot_cumulative_series_cpue_by_season"
+    effort_file_like = read_file_as_buffer(input_path)
+    files = {
+        "file": (input_path, effort_file_like, "text/csv"),
+    }
+    extension = output_path.split(".")[-1]
+    response = requests.post(url, files=files, data={"format": extension})
+    write_response_content(output_path, response)
+    print(response.status_code)
+    return response
 
 
 @cli.command()

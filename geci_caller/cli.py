11a743a8200c9d2e9d2ecbcf1bb915bcb0a9dff6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-04-15 18:56:39 +0000

    ✅ Pass tests

--- geci_caller/cli.py
+++ geci_caller/cli.py
@@ -315,6 +315,14 @@ def plot_cpue_vs_cum_captures(
 
 
 @cli.command()
+def write_instantaneous_and_cumulative_cpue_time_series_plot(
+    input_path: str = typer.Option(help="Path of input data"),
+    output_path: str = typer.Option(help="Path of figure to write"),
+):
+    pass
+
+
+@cli.command()
 def version():
     version = gc.__version__
     print(version)
